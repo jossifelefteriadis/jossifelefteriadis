2bd75079ca01f20bd52bb79b905db5c49bf37b83
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2022-01-31 20:48:40 +0100

    add privacy policy endpoint for virus escape app

--- jossifelefteriadis/src/components/Body.js
+++ jossifelefteriadis/src/components/Body.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Opener from './Opener'
-import Skills from './Skills'
-import About from './About'
-import Contact from './Contact'
+import Opener from "./Opener";
+import Skills from "./Skills";
+import About from "./About";
+import Contact from "./Contact";
+import VirusEscapePrivacyPolicy from "./VirusEscapePP";
 
 const Body = () => (
-  <section className='Container'>
+  <section className="Container">
     <Opener />
     <Skills />
     <About />
     <Contact />
+    <Route path="/virus-escape-privacy-policy">
+      <VirusEscapePrivacyPolicy />
+    </Route>
   </section>
-)
+);
 
-export default Body
+export default Body;
