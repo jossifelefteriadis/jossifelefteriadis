1e670e80f3b75d149f89f356ce5181f3ca06e867
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2022-03-22 14:00:27 +0100

    Update header

--- jossifelefteriadis/src/components/Header.js
+++ jossifelefteriadis/src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import logo from '../images/jossif.png'
+import React from "react";
+import logo from "../images/jossif.png";
 
 const Header = () => (
-  <header className='App-header'>
-    <img src={logo} className='App-logo' alt='logo' />
-    <p>Junior Full Stack Developer</p>
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>JavaScript Fullstack Developer | Web3</p>
   </header>
-)
+);
 
-export default Header
+export default Header;
