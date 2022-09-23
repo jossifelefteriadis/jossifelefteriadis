d39edb6dd88c241c07f99eea0cf42c47cd6463bb
Jossif Elefteriadis <jossif_@hotmail.com>
2022-09-23 14:04:06 +0200

    create main component

---
+++ my-app/components/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <section className="w-full h-screen text-center">
+      <section className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <section>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LETS BUILD SOMETHING
+          </p>
+        </section>
+      </section>
+    </section>
+  );
+}
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 // import Link from "next/link";
 
 import Nav from "../components/nav";
+import Main from "../components/main";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
+      <Main />
     </div>
   );
 }
