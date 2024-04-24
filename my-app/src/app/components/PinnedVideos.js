18138ac669cc0299812b19e7ab109c742062f465
Jossif Elefteriadis <jossif_@hotmail.com>
2024-04-24 13:14:35 +0200

    Add images and links for videos and placeholders for projects

--- my-app/src/app/components/PinnedVideos.js
+++ my-app/src/app/components/PinnedVideos.js
@@ -1,9 +1,61 @@
-import Image from "next/image";
+import { Image } from "@nextui-org/react";
+import { FaFilm, FaScrewdriverWrench } from "react-icons/fa6";
 
 export default function PinnedVideos() {
   return (
-    <section className="w-full flex justify-between items-center">
-      carousel here
+    <section className="w-11/12 h-[600px] flex flex-col justify-evenly pt-10">
+      <section className="flex justify-evenly py-10 rounded-xl relative">
+        <a href="https://www.youtube.com/watch?v=WVEqX8DL4KE" target="_blank">
+          <Image
+            isZoomed
+            src="/assets/blur-marketplace.png"
+            alt="Blur Marketplace"
+            width={300}
+            className="rounded-xl cursor-pointer"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=0NVxgEPoxV8" target="_blank">
+          <Image
+            isZoomed
+            src="/assets/lens-wallet-tracker.png"
+            alt="Lens Wallet Tracker"
+            width={300}
+            className="rounded-xl cursor-pointer"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=SgtR44Umj1c" target="_blank">
+          <Image
+            isZoomed
+            src="/assets/t3rn-audit.png"
+            alt="t3rn Audit"
+            width={300}
+            className="rounded-xl cursor-pointer"
+          />
+        </a>
+      </section>
+      <section className="flex justify-evenly py-10 rounded-xl relative">
+        <Image
+          isZoomed
+          src="/assets/blur-marketplace.png"
+          alt="Blur Marketplace"
+          width={300}
+          className="rounded-xl cursor-pointer"
+        />
+        <Image
+          isZoomed
+          src="/assets/t3rn-audit.png"
+          alt="t3rn Audit"
+          width={300}
+          className="rounded-xl cursor-pointer"
+        />
+        <Image
+          isZoomed
+          src="/assets/blur-marketplace.png"
+          alt="Blur Marketplace"
+          width={300}
+          className="rounded-xl cursor-pointer"
+        />
+      </section>
     </section>
   );
 }
