af359cfc84c805632f81611f38d3fd64d5995ef8
Jossif Elefteriadis <jossif_@hotmail.com>
2024-04-25 20:52:36 +0200

    change font

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
 import { NextUIProvider } from "@nextui-org/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSant = DM_Sans({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={dmSant.className}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
