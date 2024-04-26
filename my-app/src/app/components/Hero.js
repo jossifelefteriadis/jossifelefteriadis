dd896f5072f713c57a1c9f5471e9da3c3345c1c5
Jossif Elefteriadis <jossif_@hotmail.com>
2024-04-26 09:28:33 +0200

    add margin

--- my-app/src/app/components/Hero.js
+++ my-app/src/app/components/Hero.js
@@ -6,7 +6,7 @@ import PinnedVideos from "./PinnedVideos";
 
 export default function Hero() {
   return (
-    <section className="w-full h-full flex justify-center">
+    <section className="w-full h-full flex justify-center mb-10">
       <section className="w-full flex flex-col md:flex-row">
         <PinnedPersonalInfo />
         <section className="w-full">
