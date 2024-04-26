ac2c8071d770809b07de9ec75707e683dcd0e349
Jossif Elefteriadis <jossif_@hotmail.com>
2024-04-26 14:28:19 +0200

    Create about section

---
+++ my-app/src/app/components/About.js
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <section className="w-1/2 px-24 text-lg text-gray-100">
+      <p>
+        Hi, I&apos;m Jossif Elefteriadis. With over 9 years of experience,
+        I&apos;ve developed a diverse skill set spanning web and fullstack
+        applications, blockchain technology, AI, CRM, and mobile application
+        development.
+        <br />
+        <br /> In recent years, I&apos;ve focused on Developer Relations, where
+        I excel in creating and delivering technical content. My work includes
+        video tutorials that clarify complex blockchain topics like
+        interoperability, smart contract portability, and consensus algorithms -
+        and demonstrating how to use onchain APIs. I&apos;ve also been leading
+        development of technical documentation, managing and nurturing a
+        community of over 50,000 members, providing detailed feedback to
+        engineers, and spearheading QA as well as PM initiatives.
+      </p>
+    </section>
+  );
+}
