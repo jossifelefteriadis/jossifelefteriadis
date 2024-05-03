0ff5125a15c5d37343f802919c31510273503012
Jossif Elefteriadis <jossif_@hotmail.com>
2024-05-03 15:41:31 +0200

    fix responsivness

--- my-app/src/app/components/PinnedVideos.js
+++ my-app/src/app/components/PinnedVideos.js
@@ -4,7 +4,7 @@ import { FaFilm, FaScrewdriverWrench } from "react-icons/fa6";
 export default function PinnedVideos() {
   return (
     <section className="xl:w-11/12 md:h-[600px] flex flex-col items-center md:justify-evenly pt-6 md:pt-10">
-      <section className="w-fit md:w-full h-[600px] md:h-fit flex flex-col md:flex-row items-center justify-between md:justify-evenly pt-10 md:pb-10 rounded-xl relative">
+      <section className="w-fit md:w-full h-[600px] md:h-fit flex flex-col md:flex-row items-center justify-between md:justify-evenly 2xl:justify-between pt-10 md:pb-10 rounded-xl relative">
         <FaFilm className="absolute top-3 left-5 text-xl" />
         <span className="absolute top-2.5 left-12">RECORDINGS</span>
         <a href="https://www.youtube.com/watch?v=WVEqX8DL4KE" target="_blank">
@@ -13,7 +13,7 @@ export default function PinnedVideos() {
             src="/assets/blur-marketplace.png"
             alt="Blur Marketplace"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
         <a href="https://www.youtube.com/watch?v=0NVxgEPoxV8" target="_blank">
@@ -22,7 +22,7 @@ export default function PinnedVideos() {
             src="/assets/lens-wallet-tracker.png"
             alt="Lens Wallet Tracker"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
         <a href="https://www.youtube.com/watch?v=SgtR44Umj1c" target="_blank">
@@ -31,11 +31,11 @@ export default function PinnedVideos() {
             src="/assets/t3rn-audit.png"
             alt="t3rn Audit"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
       </section>
-      <section className="w-fit md:w-full h-[600px] md:h-fit flex flex-col md:flex-row items-center justify-between md:justify-evenly pt-10 mt-10 md:mt-0 md:pb-10 rounded-xl relative">
+      <section className="w-fit md:w-full h-[600px] md:h-fit flex flex-col md:flex-row items-center justify-between md:justify-evenly 2xl:justify-between pt-10 mt-10 md:mt-0 md:pb-10 rounded-xl relative">
         <FaScrewdriverWrench className="absolute top-3 left-5 text-xl" />
         <span className="absolute top-2.5 left-12">PROJECTS</span>
         <a href="https://www.youtube.com/watch?v=WVEqX8DL4KE" target="_blank">
@@ -44,7 +44,7 @@ export default function PinnedVideos() {
             src="/assets/blur-marketplace.png"
             alt="Blur Marketplace"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
         <a href="https://www.youtube.com/watch?v=0NVxgEPoxV8" target="_blank">
@@ -53,7 +53,7 @@ export default function PinnedVideos() {
             src="/assets/lens-wallet-tracker.png"
             alt="Lens Wallet Tracker"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
         <a href="https://www.youtube.com/watch?v=SgtR44Umj1c" target="_blank">
@@ -62,7 +62,7 @@ export default function PinnedVideos() {
             src="/assets/t3rn-audit.png"
             alt="t3rn Audit"
             width={300}
-            className="md:w-[170px] lg:w-[200px] xl:w-[300px] rounded-xl cursor-pointer"
+            className="md:w-[170px] lg:w-[200px] xl:w-[300px] 2xl:w-[350px] rounded-xl cursor-pointer"
           />
         </a>
       </section>
