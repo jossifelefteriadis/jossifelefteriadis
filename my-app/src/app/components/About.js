809ca8609285955a232b234f5d61726c99e53fd4
Jossif Elefteriadis <jossif_@hotmail.com>
2024-05-23 13:54:10 +0200

    update About

--- my-app/src/app/components/About.js
+++ my-app/src/app/components/About.js
@@ -2,10 +2,10 @@ export default function About() {
   return (
     <section className="w-full md:w-11/12 px-8 md:px-24 text-lg text-gray-100">
       <p>
-        Hi, I&apos;m Jossif Elefteriadis. With over 9 years of experience,
-        I&apos;ve developed a diverse skill set spanning web and fullstack
-        applications, blockchain technology, AI, CRM, and mobile application
-        development.
+        Hi, I&apos;m Jossif Elefteriadis. With over 9 years of development
+        experience, I&apos;ve developed a diverse skill set spanning web and
+        fullstack applications, blockchain technology, AI, CRM, and mobile
+        application development.
         <br />
         <br /> In recent years, I&apos;ve focused on Developer Relations, where
         I excel in creating and delivering technical content. My work includes
