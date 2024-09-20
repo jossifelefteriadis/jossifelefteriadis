7cbe3a6215de7883aa9e11b9abafaf83147cf008
Jossif Elefteriadis <jossif_@hotmail.com>
2024-09-20 20:31:45 +0200

    update about section and libs

--- my-app/src/app/components/About.js
+++ my-app/src/app/components/About.js
@@ -3,18 +3,22 @@ export default function About() {
     <section className="w-full md:w-11/12 px-8 md:px-24 text-lg text-gray-100">
       <p>
         Hi, I&apos;m Jossif Elefteriadis. With over 9 years of development
-        experience, I&apos;ve developed a diverse skill set spanning web and
-        fullstack applications, blockchain technology, AI, CRM, and mobile
-        application development.
+        experience, I&apos;ve built a versatile skill set across web and
+        fullstack applications, blockchain technology, AI, CRM systems, and
+        mobile app development. My expertise spans both emerging technologies
+        and enterprise solutions, driving innovative projects from concept to
+        deployment.
         <br />
-        <br /> In recent years, I&apos;ve focused on Developer Relations, where
-        I excel in creating and delivering technical content. My work includes
-        video tutorials that clarify complex blockchain topics like
-        interoperability, smart contract portability, and consensus algorithms -
-        and demonstrating how to use onchain APIs. I&apos;ve also been leading
-        development of technical documentation, managing and nurturing a
-        community of over 50,000 members, providing detailed feedback to
-        engineers, and spearheading PM as well as QA initiatives.
+        <br /> In recent years, I&apos;ve specialized in Developer Relations,
+        excelling in creating and delivering engaging technical content. My work
+        spans video tutorials simplifying complex blockchain concepts like
+        interoperability, smart contract portability, and consensus algorithms,
+        while also showcasing onchain API integrations. I&apos;ve led the
+        development of comprehensive technical documentation, managed and grown
+        a community of 50,000+ members, provided actionable feedback to
+        engineers, and driven key product management initiatives, from strategy
+        to execution, ensuring alignment between technical teams and user
+        experience goals.
       </p>
     </section>
   );
